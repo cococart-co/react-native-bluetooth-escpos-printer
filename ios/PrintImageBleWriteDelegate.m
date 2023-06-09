e4de0d6bdff2e097cf90f80eb5879f7b58f76532
aakash <aakash@cococart.co>
2023-06-09 16:43:11 +0800

    [update] - revert sleep intervals for ios printing

--- ios/PrintImageBleWriteDelegate.m
+++ ios/PrintImageBleWriteDelegate.m
@@ -28,6 +28,7 @@
             initPrinter[4]=10;
             [RNBluetoothManager writeValue:[NSData dataWithBytes:initPrinter length:5] withDelegate:self];
             _now = -1;
+            [NSThread sleepForTimeInterval:0.01f];
         }else {
             [self print];
         }
@@ -52,6 +53,7 @@
             [RNBluetoothManager writeValue:subData withDelegate:self];
         //}
         _now = _now+sizePerLine;
+        [NSThread sleepForTimeInterval:0.01f];
         
     }
     //}while(_now<[_toPrint length]);
